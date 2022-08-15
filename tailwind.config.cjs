e162e062d474aa914e9cd4402e18e3417c5dc420
MarcellusCodes <MarcellusCodes@gmail.com>
2022-08-15 16:06:51 +0200

    Added Theme, Fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      cta: {
+        blue: "#0099ff",
+        sky: "#00bbff",
+      },
+      black: "#1F2023",
+      grey: "#dbdde1",
+    },
+    fontFamily: {
+      primary: ["Amiko", "sans-serif"],
+      secondary: ["Tajawal", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
