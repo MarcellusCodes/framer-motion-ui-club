3fabce7baa5f211a914110795e65b20d507e7d7f
MarcellusCodes <MarcellusCodes@gmail.com>
2022-08-16 08:04:41 +0200

    Fixed tailwind config structure, added favicon, folder for posts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,12 +3,14 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
-      cta: {
-        blue: "#0099ff",
-        sky: "#00bbff",
+      colors: {
+        cta: {
+          blue: "#0099ff",
+          sky: "#00bbff",
+        },
+        black_primary: "#ccc",
+        grey: "#dbdde1",
       },
-      black: "#1F2023",
-      grey: "#dbdde1",
     },
     fontFamily: {
       primary: ["Amiko", "sans-serif"],
