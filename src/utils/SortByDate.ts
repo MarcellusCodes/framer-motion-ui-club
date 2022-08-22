bd4b6e589bc6accb87f9d62b9834ec14a780a427
MarcellusCodes <MarcellusCodes@gmail.com>
2022-08-22 13:07:51 +0200

    Added: first Post to Heading in Index, removed first post from posts, sort by date

--- src/utils/SortByDate.ts
+++ src/utils/SortByDate.ts
@@ -1,8 +1,9 @@
-const sortByDate = (posts) => {
+import PostProps from "../constants/interfaces/PostProps";
+const sortByDate = (posts: PostProps) => {
   return posts.sort(
     (a, b) =>
-      new Date(b.frontmatter.pubDate).valueOf() -
-      new Date(a.frontmatter.pubDate).valueOf()
+      new Date(b.frontmatter.created_at).valueOf() -
+      new Date(a.frontmatter.created_at).valueOf()
   );
 };
 export default sortByDate;
